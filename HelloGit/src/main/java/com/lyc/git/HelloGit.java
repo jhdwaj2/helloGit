1bfece53f1f158d3dddc6ac64393a44ef3772ca7
Lou <1146296739@qq.com>
2022-04-21 15:55:14 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/lyc/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.lyc.git;
+
+/**
+ * Date: 2022/4/21
+ * Author: 3378
+ * Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
