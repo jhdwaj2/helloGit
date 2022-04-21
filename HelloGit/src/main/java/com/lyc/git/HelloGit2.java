5a2bc9d1c8828efc1671e0ab0b5e9bf826437335
jhdwaj2 <95571714+jhdwaj2@users.noreply.github.com>
2022-04-21 16:39:41 +0800

    Update HelloGit2.java

--- HelloGit/src/main/java/com/lyc/git/HelloGit2.java
+++ HelloGit/src/main/java/com/lyc/git/HelloGit2.java
@@ -5,7 +5,7 @@ package com.lyc.git;
  * Author: 3378
  * Description:
  */
-public class HelloGit {
+public class HelloGit2 {
     public static void main(String[] args) {
         System.out.println("在线添加新文件");
         
